0425c3607c5d327a2bbf822644764da379242f7a
fe-sts <felipe.rinaldini@gmail.com>
2020-10-31 20:22:29 -0300

    Criada a Classe Pessoa Criado Método cumprimentar Criado Atributos de instância nome e idade Criado Atributo Completo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,18 +1,26 @@
-class Pessoa: #escrita camel case
-    def __init__(self,nome = None, idade = 34 ): #criando Parametro (nome, idade)
+class Pessoa:  # escrita camel case
+    def __init__(self, *filhos, nome=None, idade=34):  # criando Parametro (nome, idade)
         self.idade = idade
-        self.nome = None   #nome é o Atributo
+        self.nome = None  # nome é o Atributo
+        self.filhos = list(filhos)
+
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
-if __name__ == '__main__':
-    p = Pessoa('Karina')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Felipe Rina'
-    print(p.nome)
-#Classe vc cria seus tipos personalisados
 
-#Metodo é uma função que pertence a uma Classe, portanto, sempre está conectada a um objeto
+if __name__ == '__main__':
+    felipe = Pessoa(nome='Felipe')
+    karina = Pessoa(felipe, nome='Karina')
+    print(Pessoa.cumprimentar(karina))
+    print(id(karina))
+    print(karina.cumprimentar())
+    print(karina.nome)
+    print(karina.idade)
+    for filho in karina.filhos:
+        print(filho.nome)
+    print(karina.filhos)
+
+# Classe vc cria seus tipos personalisados
+
+# Metodo é uma função que pertence a uma Classe, portanto, sempre está conectada a um objeto
