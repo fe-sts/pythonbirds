ecd308069ff2f13d8362de51884c51ec310139bd
fe-sts <felipe.rinaldini@gmail.com>
2020-10-31 18:25:17 -0300

    Criada a Classe Pessoa Criado Método cumprimentar Criado Atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,13 +1,18 @@
 class Pessoa: #escrita camel case
+    def __init__(self,nome = None, idade = 34 ): #criando Parametro (nome, idade)
+        self.idade = idade
+        self.nome = None   #nome é o Atributo
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Karina')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
-
+    print(p.nome)
+    p.nome = 'Felipe Rina'
+    print(p.nome)
 #Classe vc cria seus tipos personalisados
 
 #Metodo é uma função que pertence a uma Classe, portanto, sempre está conectada a um objeto
